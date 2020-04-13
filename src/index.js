ff2892e35383d1e206920899315aff8c98eee46d
liang <347733121@qq.com>
2020-04-13 23:04:28 +0800

    first commit!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import Index from "./components/index/index";
+import * as serviceWorker from "./serviceWorker";
+import Register from "./components/user/register/register";
+import PostList from "./test/postList";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducers from "./redux/rootReducer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AdminIndex from "./components/admin/adminIndex/adminIndex";
 
+let store = createStore(rootReducers);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={store}>
+      <Switch>
+        <Route path="/admin" component={AdminIndex} />
+        <Route path="/index" component={Index} />
+        <Route path="/" component={Index} />
+      </Switch>
+    </Provider>
+  </BrowserRouter>,
+
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
